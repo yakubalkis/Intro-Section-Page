5b1bd9a87f51299fee1e4065efeab8216ec60c83
yakubalkis <yakupalkiis@gmail.com>
2022-07-08 23:16:31 +0300

    Responsive Design partially completed.

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const doBlack = document.querySelectorAll('.nav-link');
 
-doBlack.forEach(function(s,i){
-    s.addEventListener('mouseover', function(){
-        if(s.classList.contains('features-menu') || s.classList.contains('company-menu')){
+doBlack.forEach(function (s, i) {
+    s.addEventListener('mouseover', function () {
+        if (s.classList.contains('features-menu') || s.classList.contains('company-menu')) {
             s.querySelector('.ul').style.visibility = 'visible';
             s.querySelector('.icons').src = "img/icon-arrow-down.svg";
             s.classList.add('mouseOver');
-        }
-        else s.classList.add('mouseOver');
+        } else s.classList.add('mouseOver');
     });
 
-    s.addEventListener('mouseout',function(){
-        if(s.classList.contains('features-menu') || s.classList.contains('company-menu')){
+    s.addEventListener('mouseout', function () {
+        if (s.classList.contains('features-menu') || s.classList.contains('company-menu')) {
             s.querySelector('.ul').style.visibility = 'hidden';
             s.querySelector('.icons').src = "img/icon-arrow-up.svg";
             s.classList.remove('mouseOver');
-        }
-        else s.classList.remove('mouseOver');
+        } else s.classList.remove('mouseOver');
     });
 });
